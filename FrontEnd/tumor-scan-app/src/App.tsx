9ac4156a659de4a45410bfa01048e9bf8ba685c5
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2026-07-06 14:09:47 +0700

    verc3

--- FrontEnd/tumor-scan-app/src/App.tsx
+++ FrontEnd/tumor-scan-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [previewUrl, setPreviewUrl] = useState('');//setter getter as string
   const [predictionResult, setPredictionResult] = useState(null);
 
-  const _localAPI = "http://localhost:8000/";
+  // const _localAPI = "http://localhost:8000/";
   const _vercelAPI = "https://pytorch-brain-tumor-conv2d.vercel.app/";
   const handleImageChange = (e:any) => { //when image is recieved,turn it into url to be use for display
     const file = e.target.files[0];
