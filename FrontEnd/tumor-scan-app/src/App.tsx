6a2da06c44e9b8c004be2241df68a57140e39534
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2026-07-06 11:41:48 +0700

    build fix

--- FrontEnd/tumor-scan-app/src/App.tsx
+++ FrontEnd/tumor-scan-app/src/App.tsx
@@ -2,67 +2,44 @@ import { useState } from 'react'
 import './App.css'
 // import ScanImageInput from './ImageInput'
 
-
-// function imageScanAPI(image){
-//   const [message, setMessage] = useState('');
-//    try {
-//       const response = await fetch('http://localhost:8000/items/', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: image,
-//       });
-
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-
-//       const data = await response.json();
-//       setMessage(data.message);
-//     } catch (error) {
-//       setMessage('Error sending data: ' + error.message);
-//     }
-//   };
-// }
-
 function App() {
   const [selectedImage, setSelectedImage] = useState(null); //getter setter
-  const [previewUrl, setPreviewUrl] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState('');//setter getter as string
   const [predictionResult, setPredictionResult] = useState(null);
 
-  const handleImageChange = (e) => { //when image is recieved,turn it into url to be use for display
+  const handleImageChange = (e:any) => { //when image is recieved,turn it into url to be use for display
     const file = e.target.files[0];
     console.log(file.type)
     if (file) {
       setSelectedImage(file);
       // Create a temporary URL for the preview
-      setPreviewUrl(URL.createObjectURL(file));
+      const prevURL = URL.createObjectURL(file);
+      setPreviewUrl(prevURL);
     }
 
   };
 
-  const apiTest = async () => {
-    try{
-       const response = await fetch('http://localhost:8000/');
-      //  const response = await fetch('https://http.cat/');
-       console.log(response)
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const result = await response.json();
-      console.log(result)
+  // const apiTest = async () => {
+  //   try{
+  //      const response = await fetch('http://localhost:8000/');
+  //     //  const response = await fetch('https://http.cat/');
+  //      console.log(response)
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     const result = await response.json();
+  //     console.log(result)
       
-    } catch (err) {
-      throw new Error('Network response error!');
-    } finally {
-      console.log('conclude API test')
-    }
-  };
+  //   } catch (err) {
+  //     throw new Error('Network response error!');
+  //   } finally {
+  //     console.log('conclude API test')
+  //   }
+  // };
 
-  const tumorPrediction = async(file) => { 
+  const tumorPrediction = async() => { 
     const formData = new FormData();
-    formData.append("file", selectedImage); // "file" must match FastAPI param name
+    formData.append("file", selectedImage ?? ''); // "file" must match FastAPI param name ; ?? '' for fallback null value
 
     const response = await fetch("http://localhost:8000/model/prediction", {
       method: "POST",
