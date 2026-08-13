6f1ecf035d2cd157a008ca092c02376459e6214f
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2026-08-13 23:54:41 +0700

    some fix

--- BackEnd/webAPI.py
+++ BackEnd/webAPI.py
@@ -32,7 +32,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-from webAPI import app
+# from webAPI import app
 
 
 #---------------------async context-------------------------
